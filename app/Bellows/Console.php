be13de3ce6d285235f9eff76286aec893f927723
Joe Tannenbaum <joe@joe.codes>
2023-02-08 15:01:42 -0500

    override choice method

--- app/Bellows/Console.php
+++ app/Bellows/Console.php
@@ -6,5 +6,16 @@ use Illuminate\Console\Concerns\InteractsWithIO;
 
 class Console
 {
-    use InteractsWithIO;
+    use InteractsWithIO {
+        choice as protected baseChoice;
+    }
+
+    public function choice($question, array $choices, $default = null, $attempts = null, $multiple = false)
+    {
+        if ($default === null && count($choices) === 1) {
+            $default = array_key_first($choices);
+        }
+
+        return $this->baseChoice($question, $choices, $default, $attempts, $multiple);
+    }
 }
