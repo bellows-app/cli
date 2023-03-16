acb6ee748356c385cf27edd58b71936da72d6445
Joe Tannenbaum <joe@joe.codes>
2023-03-15 20:40:59 -0400

    more maintainable

--- app/Plugins/QueueWorker.php
+++ app/Plugins/QueueWorker.php
@@ -2,7 +2,6 @@
 
 namespace Bellows\Plugins;
 
-use Bellows\Data\DefaultEnabledDecision;
 use Bellows\Data\Worker;
 use Bellows\Plugin;
 
@@ -26,79 +25,96 @@ class QueueWorker extends Plugin
         }
 
         do {
-            $connection = $this->console->askRequired('Connection', $localConnection);
+            $connection = $this->console->anticipateRequired(
+                'Connection',
+                ['database', 'sqs', 'redis', 'beanstalkd'],
+                $localConnection
+            );
             $queue = $this->console->ask('Queue', 'default');
 
-            $params = [
-                'timeout' => [
-                    'label' => 'Maximum Seconds Per Job',
-                    'value' => 0,
-                ],
-                'sleep' => [
-                    'label' => 'Rest Seconds When Empty',
-                    'value' => 60,
-                    'required' => true,
-                ],
-                'processes' => [
-                    'label' => 'Number of Processes',
-                    'value' => 1,
-                ],
-                'stopwaitsecs' => [
-                    'label' => 'Graceful Shutdown Seconds',
-                    'value' => 10,
-                ],
-                'daemon' => [
-                    'label' => 'Run Worker As Daemon',
-                    'value' => false,
-                ],
-                'force' => [
-                    'label' => 'Always Run, Even In Maintenance Mode',
-                    'value' => false,
-                ],
-                'tries' => [
-                    'label' => 'Maximum Tries',
-                    'value' => null,
-                ],
-            ];
-
-            $this->console->table(
-                ['Option', 'Value'],
-                collect($params)->map(function ($item) {
-                    $value = $item['value'];
-
-                    if (is_bool($value)) {
-                        $value = $value ? 'true' : 'false';
-                    } else if (is_null($value)) {
-                        $value = '-';
-                    }
-
-                    return [$item['label'], $value];
-                })->toArray(),
-            );
-
-            if (!$this->console->confirm('Defaults look ok?', true)) {
-                foreach ($params as $key => $item) {
-                    if (is_bool($item['value'])) {
-                        $value = $this->console->confirm($item['label'], $item['value']);
-                    } else {
-                        $value = $this->console->askForNumber($item['label'], $item['value'], $item['required'] ?? false);
-                    }
-
-                    $params[$key]['value'] = $value;
-                }
-            }
+            $params = $this->getParams();
 
             $worker = array_merge([
                 'connection' => $connection,
-                'queue' => $queue,
-            ], collect($params)->mapWithKeys(function ($item, $key) {
-                return [$key => $item['value']];
-            })->toArray());
+                'queue'      => $queue,
+            ], collect($params)->mapWithKeys(
+                fn ($item, $key) => [$key => $item['value']]
+            )->toArray());
 
             $this->queueWorkers[] = Worker::from($worker);
 
             $addAnother = $this->console->confirm('Do you want to add another queue worker?');
+
+            // If we're adding another, we don't want to use default,
+            // just offer that the first time
+            $localConnection = null;
         } while ($addAnother);
+    }
+
+    protected function getParams(): array
+    {
+        $params = [
+            'timeout' => [
+                'label' => 'Maximum Seconds Per Job',
+                'value' => 0,
+            ],
+            'sleep' => [
+                'label' => 'Rest Seconds When Empty',
+                'value' => 60,
+                'required' => true,
+            ],
+            'processes' => [
+                'label' => 'Number of Processes',
+                'value' => 1,
+            ],
+            'stopwaitsecs' => [
+                'label' => 'Graceful Shutdown Seconds',
+                'value' => 10,
+            ],
+            'daemon' => [
+                'label' => 'Run Worker As Daemon',
+                'value' => false,
+            ],
+            'force' => [
+                'label' => 'Always Run, Even In Maintenance Mode',
+                'value' => false,
+            ],
+            'tries' => [
+                'label' => 'Maximum Tries',
+                'value' => null,
+            ],
+        ];
+
+        $this->console->table(
+            ['Option', 'Value'],
+            collect($params)->map(function ($item) {
+                $value = $item['value'];
+
+                if (is_bool($value)) {
+                    $value = $value ? 'true' : 'false';
+                } else if (is_null($value)) {
+                    $value = '-';
+                }
+
+                return [$item['label'], $value];
+            })->toArray(),
+        );
+
+        if ($this->console->confirm('Defaults look ok?', true)) {
+            return $params;
+        }
+
+        foreach ($params as $key => $item) {
+            if (is_bool($item['value'])) {
+                $value = $this->console->confirm($item['label'], $item['value']);
+            } else {
+                $value = $this->console->askForNumber($item['label'], $item['value'], $item['required'] ?? false);
+            }
+
+            $params[$key]['value'] = $value;
+        }
+
+        return $params;
     }
 
     public function workers(): array
