ed20725b31c47e6dd8f3e2bb1504d55d00515a28
Joe Tannenbaum <joe@joe.codes>
2023-03-23 14:13:32 -0400

    fathom test

---
+++ tests/Unit/FathomAnalyticsTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use Bellows\Plugins\FathomAnalytics;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+
+it('can choose an app from the list', function () {
+    $mock = $this->plugin()
+        ->expectsQuestion('Select account', 'joe')
+        ->expectsConfirmation('Create new Fathom Analytics site?', 'no')
+        ->expectsQuestion('Choose a site', 'Forge It Test')
+        ->setup();
+
+    $plugin = app(FathomAnalytics::class);
+
+    $plugin->setup();
+
+    $mock->validate();
+
+    expect($plugin->environmentVariables())->toEqual([
+        'FATHOM_SITE_ID' => 'SKPAQITB',
+    ]);
+})->group('plugin');
+
+it('can create a new app', function () {
+    Http::fake([
+        'sites' => Http::response([
+            'id'      => '789',
+            'name'    => 'Test App',
+        ]),
+    ]);
+
+    $mock = $this->plugin()
+        ->expectsQuestion('Select account', 'joe')
+        ->expectsConfirmation('Create new Fathom Analytics site?', 'yes')
+        ->expectsQuestion('Enter your site name', 'Test App')
+        ->setup();
+
+    $plugin = app(FathomAnalytics::class);
+
+    $plugin->setup();
+
+    $mock->validate();
+
+    expect($plugin->environmentVariables())->toEqual([
+        'FATHOM_SITE_ID' => '789',
+    ]);
+})->group('plugin');
