4cd3891775809b2e84c0bbd27058c484657533bc
Joe Tannenbaum <joe@joe.codes>
2023-03-23 13:46:20 -0400

    bugsnag php tests

--- tests/Unit/BugsnagPHPTest.php
+++ tests/Unit/BugsnagPHPTest.php
@@ -1,126 +1,83 @@
 <?php
 
-use Bellows\Config;
-use Bellows\Console;
-use Bellows\Data\ForgeServer;
-use Bellows\Data\ProjectConfig;
 use Bellows\Plugins\BugsnagPHP;
-use Illuminate\Console\BufferedConsoleOutput;
-use Illuminate\Console\OutputStyle;
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
-use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+use Illuminate\Support\Str;
 
 beforeEach(function () {
-    $this->app->bind(
-        Config::class,
-        fn () => new Config(__DIR__ . '/../stubs/config'),
-    );
-
-    $this->app->bind(OutputInterface::class, function () {
-        return new BufferedConsoleOutput();
-    });
-
-    $this->app->bind(InputInterface::class, function () {
-        return new ArgvInput();
-    });
-
-    $this->app->bind(ProjectConfig::class, function () {
-        return new ProjectConfig(
-            isolatedUser: 'tester',
-            repositoryUrl: 'bellows/tester',
-            repositoryBranch: 'main',
-            phpVersion: '8.1',
-            phpBinary: 'php81',
-            projectDirectory: __DIR__ . '/../stubs/plugins/bugsnag',
-            domain: 'bellowstester.com',
-            appName: 'Bellows Tester',
-            secureSite: true,
-        );
-    });
-
-    $this->app->bind(ForgeServer::class, function () {
-        return ForgeServer::from([
-            'id'         => 123,
-            'name'       => 'test-server',
-            'type'       => 'php',
-            'ip_address' => '123.123.123.123',
-        ]);
-    });
-
-    $this->app->bind(
-        Console::class,
-        function () {
-            $console = new Console();
-
-            $console->setOutput(app(OutputStyle::class));
-
-            return $console;
-        }
-    );
-
-    Http::preventStrayRequests();
+    installComposerPackage('bugsnag/bugsnag-laravel');
 });
 
 it('can choose an app from the list', function () {
-    Http::fake([
-        'user/organizations?per_page=1' => Http::response([
-            [
-                'id' => '123',
-            ],
-        ]),
-        'user/organizations' => Http::response([
-            [
-                'id' => '123',
-            ],
-        ]),
-        'organizations/123/projects?per_page=100' => Http::response([
-            [
-                'api_key' => '456',
-                'name'    => 'Test App',
-                'type'    => 'php',
-            ],
-            [
-                'api_key' => '789',
-                'name'    => 'Test App 2',
-                'type'    => 'php',
-            ],
-        ]),
-    ]);
+    installComposerPackage('laravel/framework');
+
+    $mock = $this->plugin()
+        ->expectsQuestion('Select account', 'joe')
+        ->expectsConfirmation('Create Bugsnag PHP Project?', 'no')
+        ->expectsQuestion('Select a Bugsnag project', 'Forge It Test')
+        ->setup();
 
     $plugin = app(BugsnagPHP::class);
 
     $plugin->setup();
 
+    $mock->validate();
+
     expect($plugin->environmentVariables())->toEqual([
-        'BUGSNAG_API_KEY' => '456',
+        'BUGSNAG_API_KEY' => '3bff5a44f63b77591cbd049356e758fb',
     ]);
 })->group('plugin');
 
-it('can create a new app', function () {
+it('can create a new app', function ($package) {
+    if ($package !== 'php') {
+        installComposerPackage($package);
+    }
+
     Http::fake([
-        'user/organizations?per_page=1' => Http::response([
-            [
-                'id' => '123',
-            ],
-        ]),
-        'user/organizations' => Http::response([
-            [
-                'id' => '123',
-            ],
-        ]),
-        'organizations/123/projects' => Http::response([
-            'api_key' => '789',
-            'name'    => 'Test App 2',
+        'projects' => Http::response([
+            'id'      => '789',
+            'name'    => 'Test App',
+            'api_key' => 'test-api-key',
         ]),
     ]);
+
+    $mock = $this->plugin()
+        ->expectsQuestion('Select account', 'joe')
+        ->expectsConfirmation('Create Bugsnag PHP Project?', 'yes')
+        ->expectsQuestion('Project name', 'Test App')
+        ->setup();
 
     $plugin = app(BugsnagPHP::class);
 
     $plugin->setup();
 
+    $mock->validate();
+
+    Http::assertSent(function (Request $request) use ($package) {
+        return Str::contains($request->url(), 'projects') && $request['type'] === match ($package) {
+            'laravel/framework' => 'laravel',
+            default             => 'php',
+        };
+    });
+
     expect($plugin->environmentVariables())->toEqual([
-        'BUGSNAG_API_KEY' => '789',
+        'BUGSNAG_API_KEY' => 'test-api-key',
+    ]);
+})->group('plugin')->with(['laravel/framework', 'php']);
+
+it('will use the .env variable if there is one', function () {
+    setInEnv('BUGSNAG_API_KEY', 'test-api-key');
+
+    $mock = $this->plugin()->expectsOutputToContain('Using existing Bugsnag PHP key from')->setup();
+
+    $plugin = app(BugsnagPHP::class);
+
+    $plugin->setup();
+
+    $mock->validate();
+
+    expect($plugin->environmentVariables())->toEqual([
+        'BUGSNAG_API_KEY' => 'test-api-key',
     ]);
 })->group('plugin');
