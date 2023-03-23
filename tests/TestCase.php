f1e28a409a944fa0b9608ed40e56b7da840c215f
Joe Tannenbaum <joe@joe.codes>
2023-03-23 13:00:38 -0400

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,9 @@
 
 namespace Tests;
 
+use Bellows\Config;
+use Bellows\Data\ForgeServer;
+use Bellows\Data\ProjectConfig;
 use Illuminate\Support\Facades\Http;
 use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
 
@@ -14,6 +17,34 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         // Http::preventStrayRequests();
+
+        $this->app->bind(
+            Config::class,
+            fn () => new Config(__DIR__ . '/stubs/config'),
+        );
+
+        $this->app->bind(ProjectConfig::class, function () {
+            return new ProjectConfig(
+                isolatedUser: 'tester',
+                repositoryUrl: 'bellows/tester',
+                repositoryBranch: 'main',
+                phpVersion: '8.1',
+                phpBinary: 'php81',
+                projectDirectory: __DIR__ . '/stubs/plugins/default',
+                domain: 'bellowstester.com',
+                appName: 'Bellows Tester',
+                secureSite: true,
+            );
+        });
+
+        $this->app->bind(ForgeServer::class, function () {
+            return ForgeServer::from([
+                'id'         => 123,
+                'name'       => 'test-server',
+                'type'       => 'php',
+                'ip_address' => '123.123.123.123',
+            ]);
+        });
     }
 
     public function plugin(): PendingPlugin
