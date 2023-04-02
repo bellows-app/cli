a3c7980b9056eab9a126296c43f7218df4da133f
Joe Tannenbaum <joe@joe.codes>
2023-04-01 20:50:14 -0400

    Update HttpTest.php

--- tests/Unit/HttpTest.php
+++ tests/Unit/HttpTest.php
@@ -150,3 +150,56 @@ it('will add a new account when add new account is selected', function ($clientM
             && $validateRequestCallback($request);
     });
 })->with('http_client_types');
+
+it('can extend an existing client', function ($clientMethod, $validateRequestCallback) {
+    $contents = File::json(base_path('tests/stubs/config/config.json'));
+
+    $contents['apiCredentials']['example-com'] = [
+        'example' => [
+            'token' => 'secretstuff',
+        ],
+    ];
+
+    File::put(
+        base_path('tests/stubs/config/config.json'),
+        json_encode($contents, JSON_PRETTY_PRINT),
+    );
+
+    FacadesHttp::preventStrayRequests();
+    FacadesHttp::fake([
+        'test/endpoint' => FacadesHttp::response(),
+        'api/testaroo' => FacadesHttp::response(),
+    ]);
+
+    $mock = $this->plugin()
+        ->doesntExpectOutput('Go ahead, get your token from the url.')
+        ->doesntExpectOutput('https://example.com/settings/tokens')
+        ->expectsChoice('Select account', 'example', ['example', 'Add new account'])
+        ->setup();
+
+    $http = app(Http::class);
+
+    $http->$clientMethod(
+        'https://example.com',
+        fn (PendingRequest $request, array $credentials) => $request->withToken($credentials['token']),
+        new AddApiCredentialsPrompt(
+            url: 'https://example.com/settings/tokens',
+            credentials: ['token'],
+            displayName: 'Example API',
+            helpText: 'Go ahead, get your token from the url.',
+        ),
+        fn (PendingRequest $request) => $request->get('test/endpoint'),
+    );
+
+    $mock->validate();
+
+    $http->extendClient('https://example.com/api', 'extended-client');
+
+    $http->client('extended-client')->get('testaroo');
+
+    FacadesHttp::assertSent(function (Request $request) use ($validateRequestCallback) {
+        return $request->url() === 'https://example.com/api/testaroo'
+            && $request->hasHeader('Authorization', 'Bearer secretstuff')
+            && $validateRequestCallback($request);
+    });
+})->with('http_client_types');
