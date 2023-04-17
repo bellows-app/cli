ee7ac0038f6319dd66f5aab993a6af6a5929f7ee
Joe Tannenbaum <joe@joe.codes>
2023-04-17 09:54:05 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,8 @@
 mv .env .env.bak
 ./bellows app:build
+# git tag -a 0.1.2 -m "0.1.2"
+# git push --tags
+# https://github.com/bellows-app/cli/tags
+# https://github.com/bellows-app/cli/actions
+# https://github.com/bellows-app/cli/releases
 mv .env.bak .env
