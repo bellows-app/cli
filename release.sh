c29494b4dcf297f45daa205d6155cc7563be3972
Joe Tannenbaum <joe@joe.codes>
2023-04-17 10:13:33 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
+
+read -p "Version number (current is $latestTag):" version
+
 mv .env .env.bak
-./bellows app:build
-# git tag -a 0.1.2 -m "0.1.2"
-# git push --tags
+./bellows app:build --build-version=$version
+mv .env.bak .env
+
+git push
+git tag -a $version -m "$version"
+git push --tags
+echo "\n"
+echo "https://github.com/bellows-app/cli/releases"
 # https://github.com/bellows-app/cli/tags
 # https://github.com/bellows-app/cli/actions
 # https://github.com/bellows-app/cli/releases
-mv .env.bak .env
