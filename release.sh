6125e11ff56e131e8140f7872858aac36d717fcf
Joe Tannenbaum <joe@joe.codes>
2023-05-15 11:08:45 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -11,6 +11,7 @@ mv .env.bak .env
 if [[ $(git status --porcelain) ]]; then
     git add builds/bellows
     git commit -m "Release $version"
+    git push
 fi
 
 git push
@@ -18,6 +19,4 @@ git tag -a $version -m "$version"
 git push --tags
 echo "\n"
 echo "https://github.com/bellows-app/cli/releases"
-# https://github.com/bellows-app/cli/tags
-# https://github.com/bellows-app/cli/actions
-# https://github.com/bellows-app/cli/releases
+echo "https://app.anystack.sh/products/989a1946-3fef-4370-bd4f-7ef6cf3c6b27/releases"
