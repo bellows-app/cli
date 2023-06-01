d282b28d329350e255cad07a4df7529638ef5d2c
Joe Tannenbaum <joe@joe.codes>
2023-06-01 13:39:54 -0400

    Create BellowsConfig.php

---
+++ app/Config/BellowsConfig.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bellows\Config;
+
+class BellowsConfig
+{
+    private static $instance = null;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new BellowsConfig();
+        }
+
+        return self::$instance;
+    }
+
+    public function path(string $path)
+    {
+        return env('HOME') . '/.bellows/' . ltrim($path, '/');
+    }
+
+    private function __clone()
+    {
+    }
+}
