ce4d54f89b67ae94f24e5c4478ecd8d74e44b2e1
Joe Tannenbaum <joe@joe.codes>
2023-06-01 13:39:52 -0400

    Update Config.php

--- app/Config.php
+++ app/Config.php
@@ -2,6 +2,7 @@
 
 namespace Bellows;
 
+use Bellows\Config\BellowsConfig;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\File;
 
@@ -11,20 +12,10 @@ class Config
 
     protected array $config;
 
-    public function __construct(?string $configDir = null)
+    public function __construct()
     {
-        if ($configDir === null) {
-            $configDir = env('HOME') . '/.bellows';
-        }
-
-        $this->path = $configDir . '/config.json';
-
-        if (!file_exists($this->path)) {
-            if (is_dir($configDir) || mkdir($configDir)) {
-                $this->createConfigFile($this->path);
-            }
-        }
-
+        $this->path = BellowsConfig::getInstance()->path('config.json');
+        $this->createConfigFileIfMissing();
         $this->cacheConfig();
     }
 
@@ -36,30 +27,40 @@ class Config
     public function set(string $key, $value): void
     {
         Arr::set($this->config, $key, $value);
-
-        file_put_contents($this->path, json_encode($this->config, JSON_PRETTY_PRINT));
-
-        // We've changed the config, re-cache it
-        $this->cacheConfig();
+        $this->writeAndRefreshCache();
     }
 
     public function remove(string $key): void
     {
         Arr::forget($this->config, $key);
+        $this->writeAndRefreshCache();
+    }
 
-        file_put_contents($this->path, json_encode($this->config, JSON_PRETTY_PRINT));
-
-        // We've changed the config, re-cache it
+    protected function writeAndRefreshCache(): void
+    {
+        $this->writeConfig();
         $this->cacheConfig();
     }
 
-    protected function createConfigFile(string $path): void
+    protected function writeConfig(): void
     {
-        file_put_contents($path, json_encode([]));
+        File::put($this->path, json_encode($this->config, JSON_PRETTY_PRINT));
     }
 
     protected function cacheConfig(): void
     {
         $this->config = File::json($this->path) ?: [];
+    }
+
+    protected function createConfigFileIfMissing(): void
+    {
+        if (File::exists($this->path)) {
+            return;
+        }
+
+        File::ensureDirectoryExists(dirname($this->path));
+
+        $this->config = [];
+        $this->writeConfig();
     }
 }
