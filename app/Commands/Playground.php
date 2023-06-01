70b95d6d7c9cbe831ea10947af0a80b451450079
Joe Tannenbaum <joe@joe.codes>
2023-06-01 11:04:05 -0400

    Update Playground.php

--- app/Commands/Playground.php
+++ app/Commands/Playground.php
@@ -12,5 +12,27 @@ class Playground extends Command
 
     public function handle()
     {
+        $process = proc_open('./bellows kickoff', [
+            ['pipe', 'r'],  // stdin is a pipe that the child will read from
+            ['pipe', 'w'],  // stdout is a pipe that the child will write to
+            ['file', __DIR__ . '/error-output.txt', 'a'], // stderr is a file to write to
+        ], $pipes);
+
+        if (is_resource($process)) {
+            // Hm, how do we get the question that we expecting?
+            fwrite($pipes[0], "gary\n");
+            fwrite($pipes[0], "dev\n");
+            fwrite($pipes[0], "new york\n");
+            fclose($pipes[0]);
+
+            echo stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
+
+            // It is important that you close any pipes before calling
+            // proc_close in order to avoid a deadlock
+            $return_value = proc_close($process);
+
+            echo "command returned $return_value\n";
+        }
     }
 }
