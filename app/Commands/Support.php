3a472c0bf152cce3fda3f573dc1c8ca2537f2d02
Joe Tannenbaum <joe@joe.codes>
2024-02-08 08:22:13 -0500

    Update Support.php

--- app/Commands/Support.php
+++ app/Commands/Support.php
@@ -2,7 +2,6 @@
 
 namespace Bellows\Commands;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Process;
 use LaravelZero\Framework\Commands\Command;
 
@@ -14,7 +13,7 @@ class Support extends Command
 
     public function handle()
     {
-        $type = $this->choice('What can we help you with?', ['Bug', 'Feedback', 'Support', 'View the Docs']);
+        $type = $this->choice('What can we help you with?', ['File an Issue', 'View the Docs']);
 
         if ($type === 'View the Docs') {
             if ($this->confirm('This will open the docs in your browser, continue?', true)) {
@@ -24,32 +23,6 @@ class Support extends Command
             return;
         }
 
-        $prompt = [
-            'Bug'      => 'Please describe the bug in detail',
-            'Feedback' => 'Love to hear it! What would you like us to know',
-            'Support'  => 'Got it, what can we help you with',
-        ];
-
-        $description = $this->askRequired($prompt[$type], null, true);
-
-        $email = $this->ask('What is your email address?');
-
-        $params = [
-            'type'        => $type,
-            'description' => $description,
-            'email'       => $email,
-        ];
-
-        $computedSignature = hash_hmac(
-            'sha256',
-            json_encode($params),
-            config('app.webhook_signing_secret'),
-        );
-
-        Http::withHeaders([
-            'Signature' => $computedSignature,
-        ])->asJson()->acceptJson()->throw()->baseUrl(config('app.url'))->post('cli/support', $params);
-
-        $this->info('Thank you for your support request. We will get back to you as soon as possible.');
+        $this->info('To file an issue, please visit: https://github.com/bellows-app/cli/issues');
     }
 }
